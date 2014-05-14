baf34847c0213e10e5ecc96c689b566e73342df3
kakty3 <kakty3.mail@gmail.com>
2014-05-15 00:33:32 +0700

    before square changing

--- xlist/main.cpp
+++ xlist/main.cpp
@@ -5,43 +5,43 @@ int Shape::n_of_shapes = 0;
 
 int main(){
     XList<Shape*> shapes;
-    Point point1(0., 2., "point_1");
-    shapes.push_back(&point1);
-    Point point2(4., 2., "point_2");
-    Point point3(4., 0., "point_3");
-    Point point4(0., 0., "point_4");
-    printf("Number of shapes = %d\n", Shape::GetCount());
+    Point * point1 = new Point(0., 2., "point_1");
+    shapes.push_back(point1);
+    Point * point2 = new Point(4., 2., "point_2");
+    shapes.push_back(point2);
+    Point * point3 = new Point(4., 0., "point_3");
+    shapes.push_back(point3);
+    Point * point4 = new Point(0., 0., "point_4");
+    shapes.push_back(point4);
+    // printf("Number of shapes = %d\n", Shape::GetCount());
 
-    Circle circle(point4, 10., "circle_1");
-    shapes.push_back(&circle);
-    printf("Number of shapes = %d\n", Shape::GetCount());
+    Circle * circle = new Circle(point4, 10., "circle_1");
+    shapes.push_back(circle);
+    // printf("Number of shapes = %d\n", Shape::GetCount());
 
-    Rect rect(point1, point2, point3, point4, "rectangle");
-    shapes.push_back(&rect);
-    printf("Number of shapes = %d\n", Shape::GetCount());
+    Rect * rect = new Rect(point1, point2, point3, point4, "rectangle");
+    shapes.push_back(rect);
+    // printf("Number of shapes = %d\n", Shape::GetCount());
 
-    Square square(point4, 4, "square");
-    shapes.push_back(&square);
-    printf("Number of shapes = %d\n", Shape::GetCount());
+    Square * square = new Square(point4, 4, "square");
+    shapes.push_back(square);
+    // printf("Number of shapes = %d\n", Shape::GetCount());
 
-    Polyline polyline("polyline");
-    polyline.AddPoint(point1);
-    polyline.AddPoint(point2);
-    polyline.AddPoint(point3);
-    polyline.AddPoint(point4);
-    shapes.push_back(&polyline);
-    printf("Number of shapes = %d\n", Shape::GetCount());
-    polyline.print();
-    printf("Number of shapes = %d\n", Shape::GetCount());
+    Polyline * polyline = new Polyline("polyline");
+    polyline->AddPoint(point1);
+    polyline->AddPoint(point2);
+    polyline->AddPoint(point3);
+    polyline->AddPoint(point4);
+    shapes.push_back(polyline);
 
     for (XList<Shape*>::iterator it = shapes.begin(); it != NULL; ++it) {
         std::cout << *it;
     }
     printf("Number of shapes = %d\n", Shape::GetCount());
     for (XList<Shape*>::iterator it = shapes.begin(); it != NULL; ++it) {
-        delete it;
+        delete *it;
+        std::cout << '\n';
     }
-    shapes.Clean();
     printf("Number of shapes = %d\n", Shape::GetCount());
     return 0;
 }
--- xlist/shapes.h
+++ xlist/shapes.h
@@ -56,8 +56,8 @@ public:
     }
     double GetX () const { return _x; }
     double GetY () const { return _y; }
-    double GetDistanceToPoint(const Point& other_point) const {
-        return sqrt(sqr(other_point.GetX() - _x) + sqr(other_point.GetY() - _y));
+    double GetDistanceToPoint(Point * other_point) const {
+        return sqrt(sqr(other_point->GetX() - _x) + sqr(other_point->GetY() - _y));
     }
 private:
     double _x;
@@ -66,7 +66,7 @@ private:
 
 class Circle : public Shape {
 public:
-    Circle(const Point& center, double radius, std::string name) : Shape(name), _center(center) {
+    Circle(Point * center, double radius, std::string name) : Shape(name), _center(center) {
         _radius = radius;
     }
     float GetSquare() const {  return (M_PI * sqr(_radius)); }
@@ -76,34 +76,33 @@ public:
                "\tcenter_y=%f\n"
                "\tradius=%f\n"
                "\tsquare=%f\n", \
-            _name.c_str(), _center.GetX(), _center.GetY(), _radius, GetSquare());
+            _name.c_str(), _center->GetX(), _center->GetY(), _radius, GetSquare());
     }
     virtual void ToStream(std::ostream & out){
         out << "name=" << _name << std::endl <<
-               "\tcenter_x=" << _center.GetX() << std::endl <<
-               "\tcenter_y=" << _center.GetY() << std::endl <<
+               "\tcenter_x=" << _center->GetX() << std::endl <<
+               "\tcenter_y=" << _center->GetY() << std::endl <<
                "\tradius=" << _radius << std::endl <<
                "\tsquare=" << GetSquare() << std::endl;
     }
 private:
-    Point _center;
+    Point * _center;
     double _radius;    
 };
 
 class Rect : public Shape {
 public:
     // we need only two points
-    Rect(const Point& top_left, const Point& top_right, const Point& bottom_right, const Point& bottom_left, std::string name)
+    Rect(Point * top_left, Point * top_right, Point * bottom_right, Point * bottom_left, std::string name)
     : Shape(name),
       _top_left(top_left), 
       _top_right(top_right),
       _bottom_right(bottom_right),
-      _bottom_left(bottom_left) {
-        }
+      _bottom_left(bottom_left) {}
 
     double GetSquare(){
-        double top_line = _top_left.GetDistanceToPoint(_top_right);
-        double side_line = _top_left.GetDistanceToPoint(_bottom_left);
+        double top_line = _top_left->GetDistanceToPoint(_top_right);
+        double side_line = _top_left->GetDistanceToPoint(_bottom_left);
         return top_line * side_line;
     };
 
@@ -115,34 +114,34 @@ public:
                "\tbottom_left_point: x=%f, y=%f\n"
                "\tsquare=%f\n",
                 _name.c_str(),
-                _top_left.GetX(), _top_left.GetY(),
-                _top_right.GetX(), _top_right.GetY(),
-                _bottom_right.GetX(), _bottom_right.GetY(),
-                _bottom_left.GetX(), _bottom_left.GetY(),
+                _top_left->GetX(), _top_left->GetY(),
+                _top_right->GetX(), _top_right->GetY(),
+                _bottom_right->GetX(), _bottom_right->GetY(),
+                _bottom_left->GetX(), _bottom_left->GetY(),
                 this->GetSquare()
         );
     }
     void ToStream(std::ostream & out){
         out << "name=" << _name << std::endl <<
-               "\ttop_left_point: x=" << _top_left.GetX() << ", y=" << _top_left.GetY() << std::endl <<
-               "\ttop_right_point: x=" << _top_right.GetX() << ", y=" << _top_right.GetY() << std::endl <<
-               "\tbottom_right_point: x=" << _bottom_right.GetX() << ", y=" << _bottom_right.GetY() << std::endl <<
-               "\tbottom_left_point: x=" << _bottom_left.GetX() << ", y=" <<  _bottom_left.GetY() << std::endl <<
+               "\ttop_left_point: x=" << _top_left->GetX() << ", y=" << _top_left->GetY() << std::endl <<
+               "\ttop_right_point: x=" << _top_right->GetX() << ", y=" << _top_right->GetY() << std::endl <<
+               "\tbottom_right_point: x=" << _bottom_right->GetX() << ", y=" << _bottom_right->GetY() << std::endl <<
+               "\tbottom_left_point: x=" << _bottom_left->GetX() << ", y=" <<  _bottom_left->GetY() << std::endl <<
                "\tsquare=" << this->GetSquare() << std::endl;
     }
 private:
-    Point _top_left;
-    Point _top_right;
-    Point _bottom_right;
-    Point _bottom_left;
+    Point * _top_left;
+    Point * _top_right;
+    Point * _bottom_right;
+    Point * _bottom_left;
 };
 
 class Square : public Rect {
 public:
-    Square(const Point& bottom_left, double side_size, std::string name) :
-        Rect(Point(bottom_left.GetX(), bottom_left.GetY() + side_size, ""),
-             Point(bottom_left.GetX() + side_size, bottom_left.GetY() + side_size, ""),
-             Point(bottom_left.GetX() + side_size, bottom_left.GetY(), ""),
+    Square(Point * bottom_left, double side_size, std::string name) :
+        Rect(new Point(bottom_left->GetX(), bottom_left->GetY() + side_size, ""),
+             new Point(bottom_left->GetX() + side_size, bottom_left->GetY() + side_size, ""),
+             new Point(bottom_left->GetX() + side_size, bottom_left->GetY(), ""),
              bottom_left,
              name) {}    
 };
@@ -151,41 +150,41 @@ class Polyline : public Shape {
 public:
     Polyline(std::string name) : Shape(name){} 
 
-    void AddPoint(const Point& point) {
+    void AddPoint(Point * point) {
         _points.push_back(point);
     }
 
     double GetLength(){
         double length = 0;
-        for (XList<Point>::iterator it = _points.begin()->GetNext(); it != NULL; ++it) {
-            XList<Point>::iterator prev = it;
+        for (XList<Point*>::iterator it = _points.begin()->GetNext(); it != NULL; ++it) {
+            XList<Point*>::iterator prev = it;
             --prev;
-            length += (*it).GetDistanceToPoint(*prev);
+            length += (*it)->GetDistanceToPoint(*prev);
         }
         return length;
     }
 
     virtual void print() {
         printf("name=%s\n", _name.c_str());
-        for (XList<Point>::iterator it = _points.begin(); it != NULL; ++it) {
+        for (XList<Point*>::iterator it = _points.begin(); it != NULL; ++it) {
             printf("-");
-            // (*it).print();
+            (*it)->print();
         }
         printf("-length=%f\n", GetLength());
     }
 
     void ToStream(std::ostream & out){
         out << "name=" << _name << std::endl;
-        for (XList<Point>::iterator it = _points.begin(); it != NULL; ++it) {
+        for (XList<Point*>::iterator it = _points.begin(); it != NULL; ++it) {
             out << "-";
-            out << "name=" << (*it).GetName() << std::endl
-                << "\tx=" << (*it).GetX() << std::endl
-                << "\ty=" << (*it).GetY() << std::endl;
+            out << "name=" << (*it)->GetName() << std::endl
+                << "\tx=" << (*it)->GetX() << std::endl
+                << "\ty=" << (*it)->GetY() << std::endl;
         }
         out << "-lenght=" << GetLength() << std::endl;
     }
 private:
-    XList<Point> _points;
+    XList<Point*> _points;
 };
 
 template <typename T>
